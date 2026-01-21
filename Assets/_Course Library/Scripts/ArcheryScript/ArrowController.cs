af58259eb81bd433a8ef2bda7efc6739f6ed5706
KaminK <k.kvvnkm@gmail.com>
2026-01-21 12:20:34 +0100

    fix: attempt to solve bug arrow
    better. observation: first 3 arrow bug at spawn point but okay afterward and shoot properly for 5 arrows then spawn no spawn afterward

--- Assets/_Course Library/Scripts/ArcheryScript/ArrowController.cs
+++ Assets/_Course Library/Scripts/ArcheryScript/ArrowController.cs
@@ -17,25 +17,44 @@ public class ArrowController : MonoBehaviour
         midPointVisual.SetActive(true);
     }
 
+    // public void ReleasedArrow(float strength)
+    // {
+    //     midPointVisual.SetActive(false);
+    //     //print($"Bow strength is {strength}");
+
+    //     GameObject arrow = Instantiate(arrowPrefab);
+    //     arrow.transform.position = arrowSpawnPoint.transform.position;
+    //     // arrow.transform.rotation = midPointVisual.transform.rotation;
+    //     Vector3 shootDir = midPointVisual.transform.right;
+
+    //     // rotate arrow so its FORWARD points along shootDir
+    //     arrow.transform.rotation = Quaternion.LookRotation(shootDir, Vector3.up);
+
+    //     Rigidbody rb = arrow.GetComponent<Rigidbody>();
+    //     // Debug.DrawRay(midPointVisual.transform.position, midPointVisual.transform.forward * 2f, Color.green, 2f);
+    //     // Debug.DrawRay(midPointVisual.transform.position, midPointVisual.transform.up * 2f, Color.blue, 2f);
+    //     // Debug.DrawRay(midPointVisual.transform.position, midPointVisual.transform.right * 2f, Color.red, 2f);
+
+    //     rb.AddForce(shootDir * strength * arrowMaxSpeed, ForceMode.Impulse);
+    // }
+
     public void ReleasedArrow(float strength)
     {
+        if (!arrowPrefab || !arrowSpawnPoint || !midPointVisual) return;
+
         midPointVisual.SetActive(false);
-        //print($"Bow strength is {strength}");
 
         GameObject arrow = Instantiate(arrowPrefab);
         arrow.transform.position = arrowSpawnPoint.transform.position;
-        // arrow.transform.rotation = midPointVisual.transform.rotation;
-        Vector3 shootDir = midPointVisual.transform.right;
 
-        // rotate arrow so its FORWARD points along shootDir
+        Vector3 shootDir = midPointVisual.transform.right;
         arrow.transform.rotation = Quaternion.LookRotation(shootDir, Vector3.up);
 
-        Rigidbody rb = arrow.GetComponent<Rigidbody>();
-        // Debug.DrawRay(midPointVisual.transform.position, midPointVisual.transform.forward * 2f, Color.green, 2f);
-        // Debug.DrawRay(midPointVisual.transform.position, midPointVisual.transform.up * 2f, Color.blue, 2f);
-        // Debug.DrawRay(midPointVisual.transform.position, midPointVisual.transform.right * 2f, Color.red, 2f);
+        Rigidbody rb = arrow.GetComponentInChildren<Rigidbody>(); //  change THIS
+        if (!rb) { Destroy(arrow); return; }                      //  add THIS
 
         rb.AddForce(shootDir * strength * arrowMaxSpeed, ForceMode.Impulse);
     }
+
 
 }
